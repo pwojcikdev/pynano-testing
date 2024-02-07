c78d9d953cbd4f77c67ea0ebd8fa9c088c678d78
Piotr Wójcik <3044353+pwojcikdev@users.noreply.github.com>
2024-02-07 16:23:34 +0100

    env prefix

--- nanotesting/env.py
+++ nanotesting/env.py
@@ -6,14 +6,14 @@ env = Env()
 env.read_env()
 
 # prefix for all docker container names
-PREFIX = env("PREFIX", default="fullnet")
+PREFIX = env("NANO_FULLNET_PREFIX", default="fullnet")
 
 # RPC
 RPC_PORT = 17076
-BASE_RPC_PORT = env.int("RPC_PORT", default=28000)
+BASE_RPC_PORT = env.int("NANO_FULLNET_RPC_PORT", default=28000)
 # REALTIME
 REALTIME_PORT = 17075
-BASE_REALTIME_PORT = env.int("REALTIME_PORT", default=38000)
+BASE_REALTIME_PORT = env.int("NANO_FULLNET_REALTIME_PORT", default=38000)
 
 if BASE_RPC_PORT == 0:
     BASE_RPC_PORT = None
@@ -24,33 +24,35 @@ BURN_ACCOUNT = "nano_1111111111111111111111111111111111111111111111111111hifc8np
 DEFAULT_REPR = BURN_ACCOUNT
 DIFFICULTY = "0000000000000000"
 
-NODE_IMAGE = env("NODE_IMAGE", default="nano-node")
-PROM_IMAGE = env("PROM_IMAGE", default="nano-prom-exporter")
-NETSHOOT_IMAGE = "nicolaka/netshoot"
+NODE_IMAGE = env("NANO_FULLNET_NODE_IMAGE", default="nano-node")
+PROM_IMAGE = env("NANO_FULLNET_PROM_IMAGE", default="nano-prom-exporter")
+NETSHOOT_IMAGE = env("NANO_FULLNET_NETSHOOT_IMAGE", default="nicolaka/netshoot")
 
 NANO_DATA_PATH = "/root/Nano"
 
-CPU_LIMIT = env.int("CPU_LIMIT", 4)
+CPU_LIMIT = env.int("NANO_FULLNET_CPU_LIMIT", 4)
 if CPU_LIMIT == 0:
     CPU_LIMIT = None
 
-RAMDISK = env.bool("RAMDISK", False)
-TCPDUMP = env.bool("TCPDUMP", False)
+RAMDISK = env.bool("NANO_FULLNET_RAMDISK", False)
+TCPDUMP = env.bool("NANO_FULLNET_TCPDUMP", False)
 
-TCPDUMP_PATH = env.path("TCPDUMP_PATH", default="~/fullnet-tcpdump/")
+TCPDUMP_PATH = env.path("NANO_FULLNET_TCPDUMP_PATH", default="/data-raid/fullnet-tcpdump/")
 
 DEFAULT_NODE_FLAGS = [
     # "disable_max_peers_per_ip",
     # "disable_max_peers_per_subnetwork",
 ]
 
-NODE_FLAGS = env("NODE_FLAGS", default="")
+NODE_FLAGS = env("NANO_FULLNET_NODE_FLAGS", default="")
 
 
 @title_bar(name="ENV INFO")
 def print_env_info():
     print("PREFIX:", PREFIX)
     print("NODE_IMAGE:", NODE_IMAGE)
+    print("PROM_IMAGE:", PROM_IMAGE)
+    print("NETSHOOT_IMAGE:", NETSHOOT_IMAGE)
     print("BASE_RPC_PORT:", BASE_RPC_PORT)
     print("BASE_REALTIME_PORT:", BASE_REALTIME_PORT)
     print("BURN_ACCOUNT:", BURN_ACCOUNT)
